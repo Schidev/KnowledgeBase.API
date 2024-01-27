cb393cc1707dc7fddbe2d5aca7c93e87016279f6
Denys Shyfrin <schifrindenis@gmail.com>
2024-01-28 01:29:54 +0200

    Finish  and some fixing.

--- Presentation/KnowledgeBase.API.WebAPI/Controllers/BaseController.cs
+++ Presentation/KnowledgeBase.API.WebAPI/Controllers/BaseController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace KnowledgeBase.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     public abstract class BaseController : ControllerBase
     {
     }
